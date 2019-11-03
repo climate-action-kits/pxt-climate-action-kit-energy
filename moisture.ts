b6dbf09c41fd3896d6f1ba69002f9d61495414d4
Micheal Friesen <michealfriesen@gmail.com>
2019-11-03 14:34:17 -0500

    Added analog read, along with arbitrary moisture levels

--- moisture.ts
+++ moisture.ts
@@ -1,22 +1,12 @@
-enum Pin {
-    P0,
-    P1,
-    P2,
-    P8,
-    P12
-}
-
 enum Mlevel {
     //% block="Very Wet"
-    VERY_WET,
+    VERY_WET = 600,
     //% block="Wet"
-    WET,
-    //% block="Neutral"
-    NEUTRAL,
+    WET = 450,
     //% block="Dry"
-    DRY,
+    DRY = 250,
     //% block="Very Dry"
-    VERY_DRY
+    VERY_DRY = 0
 }
 
 namespace mositure {
@@ -24,12 +14,26 @@ namespace mositure {
      * Returns the value of the moisture sensor at a specific pin.
      */
     //% block="Read moisture level at pin $pin"
-    export function getMoisture(pin: Pin) {
-        // TODO: Return the pin reading at the listed pin.
+    export function getMoisture(pin: AnalogPin) : Mlevel {
+        let moistureLevel = pins.analogReadPin(pin)
+        if (moistureLevel > Mlevel.VERY_WET) {
+            return Mlevel.VERY_WET
+        }
+        else if (moistureLevel > Mlevel.WET) {
+            return Mlevel.WET
+        }
+        else if (moistureLevel > Mlevel.DRY) {
+            return Mlevel.DRY
+        }
+        else {
+            return Mlevel.VERY_DRY   
+        }
     }
 
-    //% block="If moisture level at $pin is $mlevel"
-    export function ifMoisture(pin: Pin, mlevel: Mlevel, handler: () => void) {
-        // TODO: Create a callback after verifying it is the level specified
-    }
+//     //% block="If moisture level at $pin is $mlevel"
+//     export function ifMoisture(pin: AnalogPin, mlevel: Mlevel, handler: () => void) {
+//         if (getMoisture(pin) > mlevel) {
+//             handler()   
+//         }
+//     }
 }
