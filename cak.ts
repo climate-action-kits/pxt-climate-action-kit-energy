d4e8c92d6c9ac80cc675ef032e00e449baa03604
Micheal Friesen <michealfriesen@gmail.com>
2019-11-03 16:40:28 -0500

    Create cak.ts

---
+++ cak.ts
@@ -0,0 +1,7 @@
+//% weight=100 color=#421C52 icon="\uf1b9"
+namespace cak {
+    export let M2_PWR: number = DigitalPin.P13
+    export let M2_DIR: number = DigitalPin.P14
+    export let M1_PWR: number = DigitalPin.P15
+    export let M1_DIR: number = DigitalPin.P16
+}
