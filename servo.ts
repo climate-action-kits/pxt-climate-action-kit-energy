691283b811431d12888ca2f638e91011f059aef5
Micheal Friesen <michealfriesen@gmail.com>
2019-11-19 00:09:44 -0500

    Improved pin selection

--- servo.ts
+++ servo.ts
@@ -13,15 +13,40 @@ enum Position {
 
 //% weight=10 color=#ab47bc icon=""
 namespace servos {
+
+    export enum ServoPin{
+        P0,
+        P1,
+        P2,
+        P8,
+        P12
+    }
+
+    /**
+     * Function used to return actual AnalogPin from enum
+     */
+    function getServoPin(pin: ServoPin): AnalogPin {
+        // Read from the pin specified from arg
+        switch (pin) {
+            case ServoPin.P0: return AnalogPin.P0
+            case ServoPin.P1: return AnalogPin.P1
+            case ServoPin.P2: return AnalogPin.P2
+            case ServoPin.P8: return AnalogPin.P8
+            case ServoPin.P12: return AnalogPin.P12
+        }
+        // Default return if something goes wrong.
+        return AnalogPin.P0
+    }
+
     /**
      * Move specified servo to the selected position
      */
     //% block
     //% blockId=servo_set block="set servo at %pin to |position: %position"
     //% weight=60
-    export function setServoPosition(pin: AnalogPin, position: Position) {
+    export function setServoPosition(pin: ServoPin, position: Position) {
         let n: number = position
-        pins.servoWritePin(pin, -n + 90)
+        pins.servoWritePin(getServoPin(pin), -n + 90)
     }
 
     /**
@@ -30,8 +55,8 @@ namespace servos {
     //% block
     //% blockId=servos_reset block="reset servo at %pin"
     //% weight=40
-    export function resetServos(pin: AnalogPin) {
-        pins.servoWritePin(pin, 90)
+    export function resetServos(pin: ServoPin) {
+        pins.servoWritePin(getServoPin(pin), 90)
     }
 
     /**
@@ -42,7 +67,7 @@ namespace servos {
     //% blockId=servo_turn_degrees block="turn servo at %pin to |degrees: %degrees"
     //% degrees.min=-90 degrees.max=90
     //% weight=60
-    export function turnServo(pin: AnalogPin, degrees: number) {
-        pins.servoWritePin(pin, -degrees + 90)
+    export function turnServo(pin: ServoPin, degrees: number) {
+        pins.servoWritePin(getServoPin(pin), -degrees + 90)
     }
 }
