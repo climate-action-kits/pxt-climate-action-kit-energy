cfad69187c6f49772f4393a9da7f26fe2b0ec914
Micheal Friesen <michealfriesen@gmail.com>
2019-11-19 00:10:11 -0500

    Improved pin selection

--- soil.ts
+++ soil.ts
@@ -1,50 +1,66 @@
 enum Mlevel {
     //% block="Very Wet"
-    VERY_WET = 750,
+    VERY_WET = 500,
     //% block="Wet"
-    WET = 500,
+    WET = 200,
     //% block="Dry"
-    DRY = 250,
+    DRY = 100,
     //% block="Very Dry"
     VERY_DRY = 0
 }
 
 //% weight=13 color=#ff6700 icon=""
 namespace soil {
+
+    export enum SoilPin {
+        P0,
+        P1,
+        P2
+    }
+
+    /**
+     * Function used to return actual AnalogPin from enum
+     */
+    function getSoilPin(pin: SoilPin): AnalogPin {
+        // Read from the pin specified from arg
+        switch (pin) {
+            case SoilPin.P0: return AnalogPin.P0
+            case SoilPin.P1: return AnalogPin.P1
+            case SoilPin.P2: return AnalogPin.P2
+        }
+        // Default return if something goes wrong.
+        return AnalogPin.P0
+    }
+
     /**
      * Returns the value of the moisture sensor at a specific pin.
      */
-    //% block="If moisture level at pin $pin is ____"
-    export function getMoisture(pin: AnalogPin): number {
-        return pins.analogReadPin(pin)
+    //% block="moisture level at pin $pin"
+    export function getMoisture(pin: SoilPin): number {
+        return pins.analogReadPin(getSoilPin(pin))
     }
 
 
     /**
-     * Runs a specific function if the moisture value is between the two passed moisture levels
+     * Runs a specific function if the moisture value is below the selected moisture level
      */
-    //% block="If moisture level at $pin is between $mlevel and $mlevel2"
-    export function ifMoisture(pin: AnalogPin, mlevel: Mlevel, mlevel2: Mlevel, handler: () => void) {
+    //% block="on moisture level at $pin is below $mlevel"
+    export function ifMoisture(pin: SoilPin, mlevel: Mlevel, handler: () => void) {
         let moistureLevel = getMoisture(pin)
-        if (moistureLevel >= mlevel) {
-            if (moistureLevel <= mlevel2) {
-                handler()
-            }
+        if (moistureLevel <= mlevel) {
+            handler()
         }
-        else if (moistureLevel >= mlevel2) {
-            if (moistureLevel <= mlevel) {
-                handler()
-            }
-        }
+
+
     }
 
     /**
-     * Displays the read moisture level on the LED grid
+     * Displays the read moisture level on a vertical bar graph up to full scale.
      */
-    //% block="moisture at pin %pin to LED"
+    //% block="display moisture level at %pin"
     //% weight=40
-    export function displayMoisture(pin: AnalogPin): void {
-        led.plotBarGraph(getMoisture(pin), 1023)
+    export function displayMoisture(pin: SoilPin): void {
+        led.plotBarGraph(getMoisture(pin), 800)
     }
 
 }
