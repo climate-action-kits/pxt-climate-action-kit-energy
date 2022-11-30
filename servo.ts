1bdd4a93d7a79e157594d41b5ede88e27e96b3aa
ross <88154489+ross-inksmith@users.noreply.github.com>
2022-11-30 17:15:06 -0500

    Naming changes, formatting tidying (#1)
    * Updating enums to prevent collisions
    
    * Formatting changes, tidying

--- servo.ts
+++ servo.ts
@@ -1,4 +1,6 @@
-enum Position {
+//% weight=10 color=#ab47bc icon="" block="Servo"
+namespace cakEnergyServos {
+  enum Position {
     //% block="up"
     UP = 85,
     //% block="half up"
@@ -9,77 +11,73 @@ enum Position {
     HALF_DOWN = -40,
     //% block="down"
     DOWN = -85
-}
+  }
 
-//% weight=10 color=#ab47bc icon="" block="Servo"
-namespace cakLandServos {
 
-    export enum ServoPin{
-        P0,
-        P1,
-        P2,
-        P8,
-        P12
-    }
+  export enum ServoPin{
+    P0,
+    P1,
+    P2,
+    P8,
+    P12
+  }
 
-    export enum MotorPin{
-        //% block="left"
-        Left,
-        //% block="right"
-        Right
-    }
+  export enum MotorPin{
+    //% block="left"
+    Left,
+    //% block="right"
+    Right
+  }
 
-    export enum Power {
-        //% block="off"
-        Off = 0,
-        //% block="on"
-        On = 1,
-    }
+  export enum Power {
+    //% block="off"
+    Off = 0,
+    //% block="on"
+    On = 1,
+  }
 
     /**
      * Enable a motor pin for access to 5v instead of the 3.3v on the rest of the breakout board
      */
     //% block
-    //% blockId=servo_set block="turn %side motor + pin %state"
+    //% blockId=servo_set_power block="turn %side motor + pin %state"
     //% weight=100
     export function motorPinPower (side: MotorPin, state: Power): void {
-        let pin : DigitalPin;
-        // Pick a pin
-        switch (side) {
-            case MotorPin.Left: pin = cakEnergy.M1_POS; break;
-            case MotorPin.Right: pin = cakEnergy.M2_POS; break;
-            default: pin = null; break;
-        }
-        if (pin) {
-            pins.digitalWritePin(pin, state);
-        }
+      let pin : DigitalPin;
+      // Pick a pin
+      switch (side) {
+        case MotorPin.Left: pin = cakEnergy.M1_POS; break;
+        case MotorPin.Right: pin = cakEnergy.M2_POS; break;
+        default: pin = null; break;
+      }
+      if (pin) {
+        pins.digitalWritePin(pin, state);
+      }
     }
 
     /**
      * Function used to return actual AnalogPin from enum
      */
     function getServoPin(pin: ServoPin): AnalogPin {
-        // Read from the pin specified from arg
-        switch (pin) {
-            case ServoPin.P0: return AnalogPin.P0
-            case ServoPin.P1: return AnalogPin.P1
-            case ServoPin.P2: return AnalogPin.P2
-            case ServoPin.P8: return AnalogPin.P8
-            case ServoPin.P12: return AnalogPin.P12
-        }
-        // Default return if something goes wrong.
-        return AnalogPin.P0
+      // Read from the pin specified from arg
+      switch (pin) {
+        case ServoPin.P0: return AnalogPin.P0
+        case ServoPin.P1: return AnalogPin.P1
+        case ServoPin.P2: return AnalogPin.P2
+        case ServoPin.P8: return AnalogPin.P8
+        case ServoPin.P12: return AnalogPin.P12
+      }
     }
 
     /**
      * Move specified servo to the selected position
      */
     //% block
-    //% blockId=servo_set block="set servo at %pin to |position: %position"
+    //% blockId=servo_set_pos block="set servo at %pin to |position: %position"
     //% weight=60
     export function setServoPosition(pin: ServoPin, position: Position) {
-        let n: number = position
-        pins.servoWritePin(getServoPin(pin), -n + 90)
+      let n: number = position
+      pins.servoWritePin(getServoPin(pin), -n + 90)
     }
 
     /**
@@ -89,7 +87,7 @@ namespace cakLandServos {
     //% blockId=servos_reset block="reset servo at %pin"
     //% weight=40
     export function resetServos(pin: ServoPin) {
-        pins.servoWritePin(getServoPin(pin), 90)
+      pins.servoWritePin(getServoPin(pin), 90)
     }
 
     /**
@@ -101,6 +99,6 @@ namespace cakLandServos {
     //% degrees.min=-90 degrees.max=90
     //% weight=60
     export function turnServo(pin: ServoPin, degrees: number) {
-        pins.servoWritePin(getServoPin(pin), -degrees + 90)
+      pins.servoWritePin(getServoPin(pin), -degrees + 90)
     }
 }
--- touch.ts
+++ touch.ts
@@ -1,58 +1,58 @@
 //% weight=10 color=#007A4B icon="\uf069" block="Touch"
-namespace cakLandTouch {
+namespace cakEnergyTouch {
 
-    export enum TouchPin {
-        P0,
-        P1,
-        P2,
-        P8,
-        P12
+  export enum TouchPin {
+    P0,
+    P1,
+    P2,
+    P8,
+    P12
+  }
+
+  let latches:TouchPin[] = [];
+
+  /**
+   * Function used to return actual DigitalPin from enum
+   */
+  function getTouchPin(pin: TouchPin): DigitalPin {
+    // Read from the pin specified from arg
+    switch (pin) {
+      case TouchPin.P0: return DigitalPin.P0
+      case TouchPin.P1: return DigitalPin.P1
+      case TouchPin.P2: return DigitalPin.P2
+      case TouchPin.P8: return DigitalPin.P8
+      case TouchPin.P12: return DigitalPin.P12
+      default: return DigitalPin.P0
     }
+  }
 
-    let latches:TouchPin[] = [];
-
-    /**
-     * Function used to return actual DigitalPin from enum
-     */
-    function getTouchPin(pin: TouchPin): DigitalPin {
-        // Read from the pin specified from arg
-        switch (pin) {
-            case TouchPin.P0: return DigitalPin.P0
-            case TouchPin.P1: return DigitalPin.P1
-            case TouchPin.P2: return DigitalPin.P2
-            case TouchPin.P8: return DigitalPin.P8
-            case TouchPin.P12: return DigitalPin.P12
-            default: return DigitalPin.P0
-        }
+  /**
+   * Flipflop style button so that only one true per press is emitted 
+   */
+  //% block="on tap at $pin"
+  export function getTap(pin: TouchPin): boolean {
+    let touch = getTouch(pin);
+    if (touch && latches.indexOf(pin) === -1) {
+      latches.push(pin);
+      return true;
+    } else if (!touch) {
+      latches = latches.filter((x) => x != pin);
     }
+    return false;
+  }
 
-    /**
-     * Flipflop style button so that only one true per press is emitted 
-     */
-    //% block="on tap at $pin"
-    export function getTap(pin: TouchPin): boolean {
-        let touch = getTouch(pin);
-        if (touch && latches.indexOf(pin) === -1) {
-           latches.push(pin);
-           return true;
-        } else if (!touch) {
-            latches = latches.filter((x) => x != pin);
-        }
-        return false;
+  /**
+   * Returns the value of the touch sensor at a specific pin.
+   * Returns true so long as button is pressed
+   */
+  //% block="on touch at $pin"
+  function getTouch(pin: TouchPin): boolean {
+    // Return bool associated with the pin reading
+    if (pins.digitalReadPin(getTouchPin(pin)) > 0) {
+      return true
     }
-
-    /**
-     * Returns the value of the touch sensor at a specific pin.
-     * Returns true so long as button is pressed
-     */
-    //% block="on touch at $pin"
-    function getTouch(pin: TouchPin): boolean {
-        // Return bool associated with the pin reading
-        if (pins.digitalReadPin(getTouchPin(pin)) > 0) {
-            return true
-        }
-        else {
-            return false
-        }
+    else {
+      return false
     }
+  }
 }
